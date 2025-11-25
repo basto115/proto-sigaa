4787e979b7c12520261f7e36e3a70a9617a9b0a2
basto115 <134301977+basto115@users.noreply.github.com>
2025-11-25 12:36:30 -0300

    css, html y js (incompleto) de matricula

---
+++ sigaa/js/matricula.js
@@ -0,0 +1,8 @@
+//dato bruto matricula
+const asignatura = [
+    { nombre: "Programación web" ,estado: "Inscrita"},
+    { nombre: "Base de datos" ,estado: "Inscrita"},
+    { nombre: "Arquitectura de sistemas" ,estado: "Disponible"},
+    { nombre: "Modelamiento de negocios" ,estado: "Disponible"},
+    { nombre: "Ética profesional" ,estado: "Disponible"}
+]
