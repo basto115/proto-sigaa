b9a83c4062e93a2160d8aa8bc3aabc60dccbb386
basto115 <134301977+basto115@users.noreply.github.com>
2025-11-26 12:25:55 -0300

    html y js de pagos

---
+++ sigaa/js/pagos.js
@@ -0,0 +1,27 @@
+//datos para pagos
+const pagos = [
+    { fecha: "2025-05-10", monto: 120000,  estado: "Pagado"},
+    { fecha: "2025-06-15", monto: 120000,  estado: "Pendiente"},
+    { fecha: "2025-07-20", monto: 120000,  estado: "Pagado"},
+    { fecha: "2025-08-05", monto: 120000,  estado: "Pendiente"}
+];
+
+const tabla = document.getElementById("tabla-pagos");
+
+//renderizar tabla pagos
+pagos.forEach(p => {
+    let fila = document.createElement("tr")
+
+    fila.innerHTML = `
+        <td>${p.fecha}</td>
+        <td>${p.monto}</td>
+        <td>${p.estado}</td>
+    `;
+
+    tabla.appendChild(fila);
+});
+
+//volver al dashboard
+function volver() {
+    window.location.href = "dashboard.html";
+}
